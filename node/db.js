d02612002f8ca3f66bffb1fd64a53f021e324897
unknown <khammarut10@Outlook.com>
2020-03-02 01:48:54 +0700

    add db

--- node/db.js
+++ node/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/lanna_db' , { useNewUrlParser: true })
+mongoose.connect('mongodb://heroku_s3xpzvh4:ndadaj1odpmiefa1kk9gn9gpcq@ds237267.mlab.com:37267/heroku_s3xpzvh4' , { useNewUrlParser: true })
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
